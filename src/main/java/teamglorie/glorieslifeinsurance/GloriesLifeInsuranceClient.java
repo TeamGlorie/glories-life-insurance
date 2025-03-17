91a170ff6d4368c4e373f117853f4dcb63897d70
MeteorOfTime <109677504+MeteorOfTime@users.noreply.github.com>
2025-03-17 22:57:26 +0800

    Custom
    小修小改一下各种设置

---
+++ src/main/java/teamglorie/glorieslifeinsurance/GloriesLifeInsuranceClient.java
@@ -0,0 +1,9 @@
+package teamglorie.glorieslifeinsurance;
+
+import net.fabricmc.api.ClientModInitializer;
+public class GloriesLifeInsuranceClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
